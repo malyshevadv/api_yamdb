539ca1e2d1b3c8f0df6673af95629c9b08e0d41e
Mike Lee <memorycloud@gmail.com>
2021-12-18 01:20:08 +0300

    Add Category, Genre, Titles models

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,20 +1,72 @@
 from django.contrib.auth import get_user_model
-from django.core.validators import MaxValueValidator, MinValueValidator 
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 User = get_user_model()
 
 
 class Category(models.Model):
-    pass
+    name = models.CharField(
+        max_length=256
+    )
+    slug = models.SlugField(
+        max_length=50,
+        unique=True
+    )
+
+    class Meta:
+        ordering = ['id']
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
 
 
 class Genre(models.Model):
-    pass
+    name = models.CharField(
+        max_length=256
+    )
+    slug = models.SlugField(
+        max_length=50,
+        unique=True
+    )
+
+    class Meta:
+        ordering = ['id']
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+    def __str__(self):
+        return self.name
 
 
 class Titles(models.Model):
-    pass
+    name = models.CharField(max_length=256)
+    year = models.PositiveIntegerField('Год издания')
+    description = models.TextField(
+        'Описание',
+        blank=True
+    )
+    genre = models.ManyToManyField(
+        'Genre',
+        related_name='titles',
+        verbose_name='Жанры'
+    )
+    category = models.ForeignKey(
+        'Category',
+        on_delete=models.SET_NULL,
+        related_name='titles',
+        verbose_name='Категория'
+    )
+
+    class Meta:
+        ordering = ['id']
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+
+    def __str__(self):
+        return self.name
 
 
 class Review(models.Model):
